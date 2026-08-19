9c1c8bb18c21c7def526b35aef50f237c232ccff
Sky Ruler <skyruler3281@gmail.com>
2026-08-19 20:03:06 +0530

    chore: clean up temporary files
    - Removed temporary test scripts and commit message text files that were accidentally tracked in Git.

--- backend/test-db.js
+++
@@ -1,9 +0,0 @@
-﻿const mongoose = require('mongoose');
-const User = require('./src/models/User');
-
-mongoose.connect('mongodb://127.0.0.1:27017/agri-iot')
-  .then(async () => {
-    const users = await User.find({});
-    console.log(users);
-    process.exit(0);
-  });
